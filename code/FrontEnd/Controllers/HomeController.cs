515d5038d193ef827ff81f48dca09c6a8d389e3a
kacefy <ciarra.balsomo@avanade.com>
2021-06-22 14:20:29 +0100

    all services coded, merge controller error at launch

--- code/FrontEnd/Controllers/HomeController.cs
+++ code/FrontEnd/Controllers/HomeController.cs
@@ -6,32 +6,20 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using FrontEnd.Models;
-
+using System.Net.Http;
 namespace FrontEnd.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        [HttpGet]
 
-        public HomeController(ILogger<HomeController> logger)
+        public async Task<IActionResult> Index()
         {
-            _logger = logger;
-        }
-
-        public IActionResult Index()
-        {
+            var mergedService = "https://localhost:44305/merge";
+            var thirdServiceResponseCall = await new HttpClient().GetStringAsync(mergedService);
+            ViewBag.responseCall = thirdServiceResponseCall;
             return View();
-        }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
---
+++ code/SuperHeroTest/Controllers/MergeControllerTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using FluentAssertions;
+using Merge.Controllers;
+using Merge.Library.Interfaces;
+namespace Merge.Controllers
+{
+    public class MergeControllerTest
+    {
+        private MergeController
+    }
+}
