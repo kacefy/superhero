0b7de5708e34ecf3e352c02caea306744ba93e77
kacefy <ciarra.balsomo@avanade.com>
2021-06-23 12:59:56 +0100

    terraform config added

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,28 +1,69 @@
+# config for terraform providers
 terraform{
     required_providers {
         azurerm = {
             source = "hashicorp/azurerm"
-            version = "~>2.31.1"
+            version = "~>2.46.0"
         }
 
     }
         
 }
-
-# config details for az terraform provider
-
+locals {
+    prefix = "ckb"
+    location ="uksouth"
+}
+variable "project_name" {
+    default = "superherogen"
+}
 provider "azurerm" {
     features{}
 }
 
-# creates resource group
-
+# create resource group
 resource "azurerm_resource_group" "rg" {
-    name = ckb-proj-two
-    location = "uksouth"
+    name = "${local.prefix}-${var.project_name}-rg"
+    location = "${local.location}"
     tags = {
         environment = "dev"
         source = "Terraform"
-        project = true 
+        project = "true" 
     }
+}
+# create service plan
+resource "azurerm_app_service_plan" "sp" {
+    name = "${local.prefix}-${var.project_name}-app-svc"
+    kind = "Windows"
+    sku{
+        tier = "Basic"
+        size = "B1"
+    }
+    resource_group_name = azurerm_resource_group.rg.name
+    location = azurerm_resource_group.rg.location
+}
+
+#config azure app services
+resource "azurerm_app_service" "svc1" {
+    name = "${local.prefix}-${var.project_name}-frontend"
+    resource_group_name= azurerm_resource_group.rg.name
+    location = azurerm_resource_group.rg.location
+    app_service_plan_id = azurerm_app_service_plan.sp.id
+}
+resource "azurerm_app_service" "svc2" {
+    name = "${local.prefix}-${var.project_name}-firstnames"
+    resource_group_name= azurerm_resource_group.rg.name
+    location = azurerm_resource_group.rg.location
+    app_service_plan_id = azurerm_app_service_plan.sp.id
+}
+resource "azurerm_app_service" "svc3" {
+    name = "${local.prefix}-${var.project_name}-secondnames"
+    resource_group_name= azurerm_resource_group.rg.name
+    location = azurerm_resource_group.rg.location
+    app_service_plan_id = azurerm_app_service_plan.sp.id
+}
+resource "azurerm_app_service" "sv4" {
+    name = "${local.prefix}-${var.project_name}-merge"
+    resource_group_name= azurerm_resource_group.rg.name
+    location = azurerm_resource_group.rg.location
+    app_service_plan_id = azurerm_app_service_plan.sp.id
 }
