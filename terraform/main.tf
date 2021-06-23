6a1f8ac83887c9fea9c5c77db27664bd182ad3c5
kacefy <ciarra.balsomo@avanade.com>
2021-06-23 16:04:17 +0100

    tf update

--- terraform/main.tf
+++ terraform/main.tf
@@ -48,22 +48,71 @@ resource "azurerm_app_service" "svc1" {
     resource_group_name= azurerm_resource_group.rg.name
     location = azurerm_resource_group.rg.location
     app_service_plan_id = azurerm_app_service_plan.sp.id
+
+    site_config {
+    dotnet_framework_version = "v5.0"
+    scm_type = "LocalGit"
+    }
+
+    app_settings = {
+        frontendServiceURL: "https://ckb-superherogen-frontend.azurewebsites.net"
+        WEBSITES_WEBDEPLOY_USE_SCM = "false"
+    }
+    tags = {
+        project = true
+    }
 }
 resource "azurerm_app_service" "svc2" {
     name = "${local.prefix}-${var.project_name}-firstnames"
     resource_group_name= azurerm_resource_group.rg.name
     location = azurerm_resource_group.rg.location
     app_service_plan_id = azurerm_app_service_plan.sp.id
+
+    site_config {
+    dotnet_framework_version = "v5.0"
+    scm_type = "LocalGit"
+    }
+     app_settings = {
+        firstnamesServiceURL: "https://ckb-superherogen-firstnames.azurewebsites.net"
+        WEBSITES_WEBDEPLOY_USE_SCM = "false"
+    }
+    tags = {
+        project = true
+    }
 }
 resource "azurerm_app_service" "svc3" {
     name = "${local.prefix}-${var.project_name}-secondnames"
     resource_group_name= azurerm_resource_group.rg.name
     location = azurerm_resource_group.rg.location
     app_service_plan_id = azurerm_app_service_plan.sp.id
+    
+    site_config {
+    dotnet_framework_version = "v5.0"
+    scm_type = "LocalGit"
+    }
+     app_settings = {
+        secondnamesServiceURL: "https://ckb-superherogen-secondnames.azurewebsites.net"
+        WEBSITES_WEBDEPLOY_USE_SCM = "false"
+    }
+    tags = {
+        project = true
+    }
 }
 resource "azurerm_app_service" "sv4" {
     name = "${local.prefix}-${var.project_name}-merge"
     resource_group_name= azurerm_resource_group.rg.name
     location = azurerm_resource_group.rg.location
     app_service_plan_id = azurerm_app_service_plan.sp.id
+
+    site_config {
+    dotnet_framework_version = "v5.0"
+    scm_type = "LocalGit"
+    }
+     app_settings = {
+        mergeServiceURL: "https://ckb-superherogen-merge.azurewebsites.net"
+        WEBSITES_WEBDEPLOY_USE_SCM = "false"
+    }
+    tags = {
+        project = true
+    }
 }
